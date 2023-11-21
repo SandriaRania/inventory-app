5fb35354a636f68627e19e605e2a3c1a495957f6
SandriaRania <sandria.rania@ui.ac.id>
2023-11-22 00:19:28 +0700

    tugas 9

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from main.views import show_main, create_item, show_xml, show_json, show_xml_by_id, show_json_by_id 
 from main.views import register, login_user, logout_user
 from main.views import edit_itemnya, delete_itemnya
-from main.views import get_items_json, add_item_ajax
+from main.views import get_items_json, add_item_ajax, create_product_flutter
 
 app_name = 'main'
 
@@ -20,4 +20,5 @@ urlpatterns = [
     path('delete/<int:id>', delete_itemnya, name='delete_itemnya'),
     path('get-items/', get_items_json, name='get_items_json'),
     path('create-item-ajax/', add_item_ajax, name='add_item_ajax'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 ]
